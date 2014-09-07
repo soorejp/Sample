c87422c5ff47077e13a929fdea3f67dc0c13ba6a
root <root@localhost.localdomain>
2014-09-08 00:38:04 +0530

    stage 1: Super Block ptr retrieved

--- filetrack.c
+++ filetrack.c
@@ -1,4 +1,11 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/fs_struct.h>
+#include <linux/path.h>
+#include <linux/dcache.h>
 #define SUCCESS 0;
 
 MODULE_AUTHOR("Soorej P <Soorej_Ponnandi@dell.com>");
@@ -9,17 +16,20 @@ MODULE_LICENSE("GPL v2");
 /* Module to track a file from kernel 
  *
  */
-
 int __init ft_init(void)
 {
-	printk(KERN_INFO "File Tracker started");
-
+	struct super_block *sb;
+	struct inode *s_inode;
+	printk(KERN_INFO "File Tracker started %d -> %s\n",current->pid,current->fs->root.dentry->d_name.name);
+	sb = current->fs->root.dentry->d_sb;
+	s_inode = ilookup(sb,3680126);
+	printk(KERN_INFO "inode info %ld\n",s_inode->i_mtime.tv_sec);
 	return SUCCESS;
 }
 
 void __exit ft_exit(void)
 {
-	printk(KERN_INFO "File Tracker exited");
+	printk(KERN_INFO "File Tracker exited %d -> %s\n",current->pid,current->comm);
 }
 
 module_init(ft_init);
